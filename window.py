95847e0e036ff75875c7181c23dd33e586f98b49
nathanychin <ilms2007@ymail.com>
2020-06-10 22:23:19 -0400

    Adding Grid System
    Added row and column attributes to widgets

--- window.py
+++ window.py
@@ -3,9 +3,12 @@ from tkinter import *
 root = Tk()
 
 # Creating widget
-myLabel = Label(root, text="Hello World")
+myLabel1 = Label(root, text="Label 1")
+myLabel2 = Label(root, text="Label 2")
 
 # Moving to screen
-myLabel.pack()
+# myLabel1.pack()
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=1)
 
 root.mainloop()
